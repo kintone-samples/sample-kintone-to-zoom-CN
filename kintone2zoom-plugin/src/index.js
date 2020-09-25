28a6f05041a418db490d49de2b5f5f7b2ade1a90
wenxi <34227953+wenxit1218@users.noreply.github.com>
2020-09-25 14:15:06 +0900

    Update index.js

--- kintone2zoom-plugin/src/index.js
+++ kintone2zoom-plugin/src/index.js
@@ -1,199 +1,235 @@
-import { zoomApi } from './zoomApi';
+import {ZoomApi} from './zoomApi';
 
 ((PLUGIN_ID) => {
-    const config = kintone.plugin.app.getConfig(PLUGIN_ID);
-    if (!('token' in config)) {
-        return;
+  // 設定値読み込み用変数
+  const config = kintone.plugin.app.getConfig(PLUGIN_ID);
+  // 設定値の読み込み
+  if (!('token' in config)) {
+    return;
+  }
+  // フィールドコードを変数に格納
+  const topic = 'topic';
+  const start_time = 'start_time';
+  const duration = 'duration';
+  const Attendees = 'Attendees';
+  const host = 'host';
+  const meetingNumber = 'meetingNumber';
+  const password = 'password';
+  const join_url = 'join_url';
+  const relatedZoomClient = 'relatedZoomClient';
+  const joinSpace = 'join';
+  const zoomSpace = 'zoom';
+
+  const token = config.token;
+  // zoomClientアプリのIDを取得
+  const zoomClientApp = kintone.app.getRelatedRecordsTargetAppId(relatedZoomClient);
+  // ホストのrole番号「1」を代入
+  const hostRole = 1;
+  // 参加者のrole番号「0」を代入
+  const attendRole = 0;
+  const zoomapi = new ZoomApi(token);
+  // ミーティングタイプは「2」の「Scheduled meeting」を指定
+  const meetingType = 2;
+  const loginUser = kintone.getLoginUser();
+
+  // 「ホスト」「参加」ボタンを表示し、zoomミーティング画面表示用のiframeを用意する処理
+  const detailHandle = event => {
+    kintone.app.record.setFieldShown(relatedZoomClient, false);
+    const zoomApp = kintone.app.getId();
+    const record = event.record;
+
+    if (document.getElementById('join') !== null) {
+      return;
     }
-    const token = config.token;
 
-    const zoomClientApp = kintone.app.getRelatedRecordsTargetAppId("relatedZoomClient");
-    const hostRole = 1;
-    const attendRole = 0;
-    const zoomapi = new zoomApi(token);
-    const meetingType = 2;
-    const loginUser = kintone.getLoginUser();
-
-    let detailHandle = event => {
-        kintone.app.record.setFieldShown('relatedZoomClient', false);
-        let zoomApp = kintone.app.getId();
-        let record = event.record;
-
-        if (document.getElementById('join') !== null) {
-            return;
-        }
-
-        let addZoomDom = zoomClientUrl => {
-            if (document.getElementById('zoom') !== null) {
-                document.getElementById('zoom').remove();
-            }
-            let zoomDiv = document.createElement("div");
-            let zoom = `<div id="zoom"><iframe style="border:none; height: 600px; width: 100%;" src= ${zoomClientUrl} 
+    // 「zoom」のスペースフィールドにzoom画面表示用のiframeを用意
+    const addZoomDom = zoomClientUrl => {
+      if (document.getElementById('zoom') !== null) {
+        document.getElementById('zoom').remove();
+      }
+      const zoomDiv = document.createElement('div');
+      const zoomIframe = `<div id="zoom"><iframe style="border:none; height: 600px; width: 100%;" src= ${zoomClientUrl} 
             sandbox="allow-forms allow-scripts allow-same-origin" allow="microphone; camera"></iframe></div>`;
-            zoomDiv.innerHTML = zoom;
-            kintone.app.record.getSpaceElement('zoom').appendChild(zoomDiv);
+      zoomDiv.innerHTML = zoomIframe;
+      kintone.app.record.getSpaceElement(zoomSpace).appendChild(zoomDiv);
+    };
+
+    // 「ホスト」フィールドにユーザーを追加する処理
+    const updateHost = () => {
+      const updateHostParams = {
+        'app': zoomApp,
+        'id': kintone.app.record.getId(),
+        'record': {
+          host: {
+            'value': [
+              {
+                'code': loginUser.code
+              }
+            ]
+          }
+        }
+      };
+      kintone.api(kintone.api.url('/k/v1/record', true), 'PUT', updateHostParams).catch(error => {
+        console.log(error);
+      });
+    };
+
+    // ホストの値を確認する処理
+    const checkTheHost = () => {
+      const checkTheHostParams = {
+        'app': zoomApp,
+        'id': kintone.app.record.getId()
+      };
+      return kintone.api(kintone.api.url('/k/v1/record', true), 'GET', checkTheHostParams).then(resp => {
+        return resp.record[host].value;
+      // }, (error, reject) => {
+      }, error => {
+        reject(error);
+      });
+    };
+
+    // 「出席者」フィールドにユーザーを追加する処理
+    const updateRealAttend = () => {
+      const updateRealAttendParams = {
+        'app': zoomApp,
+        'id': kintone.app.record.getId()
+      };
+      kintone.api(kintone.api.url('/k/v1/record', true), 'GET', updateRealAttendParams).then(resp => {
+        const AttendeesValue = resp.record[Attendees].value;
+        for (const value of AttendeesValue) {
+          if ('code' in value && value.code === loginUser.code) {
+            return;
+          }
         }
 
-        let updateHost = () => {
-            let params = {
-                "app": zoomApp,
-                "id": kintone.app.record.getId(),
-                "record": {
-                    "host": {
-                        "value": [
-                            {
-                                "code": loginUser.code
-                            }
-                        ]
-                    }
-                }
-            };
-            kintone.api(kintone.api.url('/k/v1/record', true), 'PUT', params).catch(error => {
-                console.log(error);
-            });
-        }
-
-        let checkTheHost = () => {
-            let params = {
-                "app": zoomApp,
-                "id": kintone.app.record.getId()
-            };
-            return kintone.api(kintone.api.url('/k/v1/record', true), 'GET', params).then(resp => {
-                let record = resp.record;
-                return record["host"].value;
-            }, error => {
-                reject(error);
-            });
-        }
-
-        let updateRealAttend = () => {
-            let params = {
-                "app": zoomApp,
-                "id": kintone.app.record.getId()
-            };
-            kintone.api(kintone.api.url('/k/v1/record', true), 'GET', params).then(resp => {
-                let record = resp.record;
-                let realAttends = record['realAttends'].value;
-                for (let value of realAttends) {
-                    if ("code" in value && value["code"] === loginUser.code) {
-                        return;
-                    }
-                }
-
-                let newAttend = {
-                    "code": loginUser.code
-                };
-                realAttends.push(newAttend);
-                let params = {
-                    "app": zoomApp,
-                    "id": kintone.app.record.getId(),
-                    "record": {
-                        "realAttends": {
-                            "value": realAttends
-                        }
-                    }
-                };
-                kintone.api(kintone.api.url('/k/v1/record', true), 'PUT', params).catch(error => {
-                    console.log(error);
-                })
-            }, error => {
-                console.log(error);
-            });
-        }
-
-        let zoom = {
-            init: function () {
-                this.addHost();
-                this.addJoin();
-            },
-            addHost() {
-                let hostButton = document.createElement('button');
-                hostButton.id = 'host';
-                hostButton.innerText = 'Host';
-                hostButton.className = 'btn btn-primary';
-                kintone.app.record.getSpaceElement('join').appendChild(hostButton);
-
-                hostButton.onclick = () => {
-                    updateHost();
-                    updateRealAttend();
-                    let meetingNumber = record.meetingNumber.value;
-                    let password = record.password.value;
-                    let zoomClientUrl = `/k/${zoomClientApp}/?meetingNumber=${meetingNumber}&password=${password}&role=${hostRole}`;
-                    addZoomDom(zoomClientUrl);
-                };
-            },
-            addJoin() {
-                let joinButton = document.createElement('button');
-                joinButton.id = 'attend';
-                joinButton.innerText = 'Attend';
-                joinButton.className = 'btn btn-info';
-                joinButton.setAttribute('style', 'margin-left:10px;');
-                kintone.app.record.getSpaceElement('join').appendChild(joinButton);
-
-                joinButton.onclick = () => {
-                    checkTheHost().then(resp => {
-                        if (resp.length == 0) {
-                            alert("Please waiting for the host");
-                            return;
-                        }
-                        updateRealAttend();
-                        let meetingNumber = record.meetingNumber.value;
-                        let password = record.password.value;
-                        let zoomClientUrl = `/k/${zoomClientApp}/?meetingNumber=${meetingNumber}&password=${password}&role=${attendRole}`;
-                        addZoomDom(zoomClientUrl);
-                    }, e => {
-                        console.log(e);
-                    })
-                };
-            }
-        }
-        zoom.init();
-    }
-    kintone.events.on('app.record.detail.show', detailHandle);
-
-    kintone.events.on('app.record.create.submit', async event => {
-        let record = event.record;
-        let data = {
-            "topic": record.topic.value,
-            "type": meetingType,
-            "start_time": record.start_time.value,
-            "duration": record.duration.value,
-            "timezone": loginUser.timezone
+        const newAttend = {
+          'code': loginUser.code
         };
-        let user = await zoomapi.getUsers().catch(e => {
-            let resp = JSON.parse(e[0]);
-            alert(resp.message);
+        AttendeesValue.push(newAttend);
+        const params = {
+          'app': zoomApp,
+          'id': kintone.app.record.getId(),
+          'record': {
+            [Attendees]: {
+              'value': AttendeesValue
+            }
+          }
+        };
+        kintone.api(kintone.api.url('/k/v1/record', true), 'PUT', params).catch(error => {
+          console.log(error);
         });
-        if (!user) return;
-        let userId = user.users[0]['id'];
-        let meetingInfo = await zoomapi.createMeeting(userId, data).catch(e => {
-            let resp = JSON.parse(e[0]);
-            alert(resp.message);
-        });
-        if (!meetingInfo) return;
-        record['meetingNumber']['value'] = meetingInfo.id;
-        record['join_url']['value'] = meetingInfo.join_url;
-        record['password']['value'] = meetingInfo.encrypted_password;
-        return event;
-    });
+      }, error => {
+        console.log(error);
+      });
+    };
 
-    kintone.events.on(['app.record.create.show', 'app.record.index.edit.show', 'app.record.edit.show'], event => {
-        let record = event.record;
-        record['meetingNumber']['disabled'] = true;
-        record['password']['disabled'] = true;
-        record['join_url']['disabled'] = true;
-        kintone.app.record.setFieldShown('realAttends', false);
-        kintone.app.record.setFieldShown('host', false);
-        return event;
-    });
+    // データを初期化し、「ホスト」「参加」ボタンを生成
+    const zoom = {
+      init: function() {
+        this.addHost();
+        this.addJoin();
+      },
+      addHost() {
+        const hostButton = document.createElement('button');
+        hostButton.id = 'host';
+        hostButton.innerText = 'Host';
+        hostButton.className = 'btn btn-primary';
+        kintone.app.record.getSpaceElement(joinSpace).appendChild(hostButton);
 
-    kintone.events.on(['app.record.detail.delete.submit', 'app.record.index.delete.submit'], event => {
-        let record = event.record;
-        let meetingId = Number(record.meetingNumber.value);
-        return zoomapi.deleteMeeting(meetingId).catch(e => {
-            let resp = JSON.parse(e[0]);
-            alert(resp.message);
-        });
+        // 「ホスト」ボタンがクリックされたときの処理
+        hostButton.onclick = () => {
+          // 「出席者」フィールドにユーザーを追加する処理
+          updateHost();
+          updateRealAttend();
+          const meetingNumberValue = record[meetingNumber].value;
+          const passwordValue = record[password].value;
+          const zoomClientUrl = `/k/${zoomClientApp}/?meetingNumber=${meetingNumberValue}&password=${passwordValue}&role=${hostRole}`;
+          // zoomミーティングの画面をiframeに表示
+          addZoomDom(zoomClientUrl);
+        };
+      },
+      addJoin() {
+        const joinButton = document.createElement('button');
+        joinButton.id = 'attend';
+        joinButton.innerText = 'Attend';
+        joinButton.className = 'btn btn-info';
+        joinButton.setAttribute('style', 'margin-left:10px;');
+        kintone.app.record.getSpaceElement(joinSpace).appendChild(joinButton);
+
+        // 「ホスト」ボタンがクリックされたときの処理
+        joinButton.onclick = () => {
+          // 「ホスト」の参加を確認する処理
+          checkTheHost().then(resp => {
+            if (resp.length === 0) {
+              alert('ホストが本ミーティングを開始するまでお待ちください');
+              return;
+            }
+            // 「出席者」フィールドにユーザーを追加
+            updateRealAttend();
+            const meetingNumberValue = record[meetingNumber].value;
+            const passwordValue = record.password.value;
+            const zoomClientUrl = `/k/${zoomClientApp}/?meetingNumber=${meetingNumberValue}&password=${passwordValue}&role=${attendRole}`;
+            // zoomミーティングの画面をiframeに表示
+            addZoomDom(zoomClientUrl);
+          }, error => {
+            console.log(error);
+          });
+        };
+      }
+    };
+    zoom.init();
+  };
+  // レコード詳細画面が表示されたときに、
+  // 「ホスト」「参加」ボタンを表示し、zoom画面表示用のiframeを用意する処理
+  kintone.events.on('app.record.detail.show', detailHandle);
+  // レコードが保存されたときに、記入された「会議名」「開始時間」「会議時間」などの情報をもとにzoomミーティングを生成
+  kintone.events.on('app.record.create.submit', async event => {
+    const record = event.record;
+    const data = {
+      [topic]: record[topic].value,
+      'type': meetingType,
+      [start_time]: record[start_time].value,
+      [duration]: record[duration].value,
+      'timezone': loginUser.timezone
+    };
+    // zoomユーザー情報を取得
+    const user = await zoomapi.getUsers().catch(error => {
+      const resp = JSON.parse(error[0]);
+      alert(resp.message);
     });
+    if (!user) return;
+    const userId = user.users[0].id;
+    // 取得したzoomユーザー情報と記入された会議情報でzoomミーティングを生成
+    const meetingInfo = await zoomapi.createMeeting(userId, data).catch(error => {
+      const resp = JSON.parse(error[0]);
+      alert(resp.message);
+    });
+    if (!meetingInfo) return;
+    record[meetingNumber].value = meetingInfo.id;
+    record[join_url].value = meetingInfo.join_url;
+    record[password].value = meetingInfo.encrypted_password;
+    return event;
+  });
+  // レコード作成/編集、一覧編集画面が表示されるときの処理
+  kintone.events.on(['app.record.create.show', 'app.record.edit.show', 'app.record.index.edit.show'], event => {
+    const record = event.record;
+    // 「会議名」「会議パスワード」「会議URL」などの情報を編集不可にする
+    record[meetingNumber].disabled = true;
+    record[password].disabled = true;
+    record[join_url].disabled = true;
+    // 「出席者」「ホスト」フィールドを非表示にする
+    kintone.app.record.setFieldShown(Attendees, false);
+    kintone.app.record.setFieldShown(host, false);
+    return event;
+  });
+
+  // レコードを削除するときに、ミーティングの削除をzoomへ反映する処理
+  kintone.events.on(['app.record.detail.delete.submit', 'app.record.index.delete.submit'], event => {
+    const record = event.record;
+    const meetingId = Number(record[meetingNumber].value);
+    return zoomapi.deleteMeeting(meetingId).catch(error => {
+      const resp = JSON.parse(error[0]);
+      alert(resp.message);
+    });
+  });
 })(kintone.$PLUGIN_ID);
-
